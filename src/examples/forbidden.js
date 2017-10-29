c6020c456584750a8e5d875b4371ab09fe2a7921
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-10-29 18:51:56 +0100

    Simplify example

--- src/examples/forbidden.js
+++ src/examples/forbidden.js
@@ -6,15 +6,7 @@ export default class Forbidden {
     const metallic = new Metallic(options)
 
     if (metallic.role === SERVER) {
-      metallic.app.use(ctx => {
-        try {
-          ctx.throw(403, STATUS_CODES[403])
-        } catch (err) {
-          ctx.status = err.status || 500
-          ctx.type = 'text/plain; charset=utf-8'
-          ctx.body = 'Something exploded'
-        }
-      })
+      metallic.app.use(async ctx => ctx.throw(403, STATUS_CODES[403]))
     }
 
     return metallic
